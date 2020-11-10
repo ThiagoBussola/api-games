b26deb54b31408f0dd22258a1b477eb089f84bd9
Thiago Bussola <thiagobussola@hotmail.com>
2020-11-10 21:11:07 +0000

    correcao

--- users/UserController.js
+++ users/UserController.js
@@ -6,8 +6,10 @@ const bcrypt = require('bcrypt')
 
 router.get("/users", async (req, res) => {
     try {
-        const usersReturned = await User.findAll()
-        usersReturned ? res.sendStatus(200).json(usersReturned) : res.sendStatus(204)
+        const usersReturned = await User.findAll({
+            attributes: ['name', 'email']
+        })
+        usersReturned ? res.status(200).json(usersReturned) : res.status(204)
     } catch (err) {
         console.log(err)
         return res.sendStatus(400)
@@ -16,15 +18,18 @@ router.get("/users", async (req, res) => {
 
 router.get("/users/:id", async (req, res) => {
     var id = parseInt(req.params.id);
-    if(isNaN(id)) {
-        res.sendStatus(400)
+    if (isNaN(id)) {
+        res.status(400)
     } else {
         try {
-            let usersReturned = User.findOne({where: { id:id }})
-            res.sendStatus(200).json({users : usersReturned})
-        } catch(err){
+            let usersReturned = await User.findOne({
+                where: { id: id },
+                attributes: ['id', 'name', 'email']
+            })
+            res.status(200).json({ users: usersReturned })
+        } catch (err) {
             console.log(err)
-            res.sendStatus(500)
+            res.status(500)
         }
     }
 })
@@ -36,12 +41,12 @@ router.post("/user", async (req, res) => {
 
     try {
 
-        await User.findOne({where:{email:email}}).then(user => {
-            if(user == undefined) {
+        await User.findOne({ where: { email: email } }).then(user => {
+            if (user == undefined) {
                 let salt = bcrypt.genSaltSync(10);
                 let hash = bcrypt.hashSync(password, salt)
-                
-                
+
+
                 User.create({
                     name: name,
                     email: email,
@@ -52,7 +57,7 @@ router.post("/user", async (req, res) => {
                 res.sendStatus(400)
             }
         })
-    } catch(err) {
+    } catch (err) {
         console.log(err)
         res.sendStatus(500)
     }
@@ -61,20 +66,20 @@ router.post("/user", async (req, res) => {
 router.delete("/user/:id", async (req, res) => {
     let id = parseInt(req.params.id)
 
-    if(isNaN(id)) {
+    if (isNaN(id)) {
         res.sendStatus(400)
     } else {
         try {
-            let found = await User.findOne({where:{ id:id }})
+            let found = await User.findOne({ where: { id: id } })
 
-            if(found) {
-                await User.destroy({where:{ id: id }})
+            if (found) {
+                await User.destroy({ where: { id: id } })
                 res.sendStatus(200)
             } else {
                 res.sendStatus(404)
             }
-            
-        } catch(err) {
+
+        } catch (err) {
             console.log(err)
         }
     }
@@ -84,59 +89,54 @@ router.put("/user/:id", async (req, res) => {
     var id = parseInt(req.params.id);
     id = parseInt(id)
 
-    if(isNaN(id)) {
+    if (isNaN(id)) {
         res.sendStatus(400)
     } else {
-        (async () => {
-            const user = await User.findByPk(id)
-
-            if(user ===  undefined) {
-                res.sendStatus(404)
-            } else {
-                let { name, email, password } = req.body;
-                if(name != undefined) {
-                    User.update({ name }, {where: { id  }})
+        try {
+            User.update({
+                name: req.body.name,
+                email: req.body.email,
+                password: bsync.hashSync(req.body.password, 10),
+            },
+                {
+                    where: {
+                        id: id
+                    }
                 }
-                if(email != undefined) {
-                    User.update({ email }, {where: { id  }})
-                }
-                if(password != undefined) {
-                    let salt = bcrypt.genSaltSync(10);
-                    let hash = bcrypt.hashSync(password, salt)
-                    
-                    User.update({ password: hash }, {where: { id  }})
-                }
-            }
-        })
+            )
+            res.send(200)
+        } catch (err) {
+            res.sendStatus(404)
+        }
     }
 })
 
-// router.post("/auth", (req, res) => {
+router.post("/auth", (req, res) => {
 
-//     var {email, password} = req.body
+    var { email, password } = req.body
 
-//     if(email != undefined) {
+    if (email != undefined) {
 
-//         var user = User.findOne({where: {email:email}})
+        var user = User.findOne({ where: { email: email } })
 
-//         if(user != undefined) {
+        if (user != undefined) {
 
-//             var correct = bcrypt.compareSync(password, user.password)
+            var correct = bcrypt.compareSync(password, user.password)
 
-//             if(user.password == correct) {
-//                 res.sendStatus = 200
-//                 res.json({token: "Token falso"})
-//             } else {
-//                 res.status = 401
-//                 res.json({err: "credenciais inválidas"})
-//             }
-//         } else {
-//             res.status = 404
-//             res.json({err: "O email enviado não existe"})            
-//         }
-//     } else {
-//         res.status(400)
-//         res.json({err: "O email enviado é inválido"})
-//     }
-// })
+            if (user.password == correct) {
+                res.sendStatus = 200
+                res.json({ token: "Token falso" })
+            } else {
+                res.status = 401
+                res.json({ err: "credenciais inválidas" })
+            }
+        } else {
+            res.status = 404
+            res.json({ err: "O email enviado não existe" })
+        }
+    } else {
+        res.status(400)
+        res.json({ err: "O email enviado é inválido" })
+    }
+})
 module.exports = router
